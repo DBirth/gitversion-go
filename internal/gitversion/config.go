312cdc271ad3f6e6c344853598b5652d7d977441
DBirth <6394283+DBirth@users.noreply.github.com>
2025-06-26 20:57:03 +0100

    refact: Agentic AI format code and run vet

--- internal/gitversion/config.go
+++ internal/gitversion/config.go
@@ -4,11 +4,11 @@ import "strings"
 
 // Config represents the structure of the GitVersion.yml file.
 type Config struct {
-	NextVersion              string `yaml:"next-version"`
-	MajorVersionBumpMessage string `yaml:"major-version-bump-message"`
-	MinorVersionBumpMessage string `yaml:"minor-version-bump-message"`
-	PatchVersionBumpMessage string `yaml:"patch-version-bump-message"`
-	Branches                 map[string]BranchConfig `yaml:"branches"`
+	NextVersion             string                  `yaml:"next-version"`
+	MajorVersionBumpMessage string                  `yaml:"major-version-bump-message"`
+	MinorVersionBumpMessage string                  `yaml:"minor-version-bump-message"`
+	PatchVersionBumpMessage string                  `yaml:"patch-version-bump-message"`
+	Branches                map[string]BranchConfig `yaml:"branches"`
 }
 
 // BranchConfig represents the configuration for a specific branch.
